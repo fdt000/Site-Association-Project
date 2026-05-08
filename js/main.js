b26e168875b4d89f23867c9a4e3cf60eab628c56
fdt000 <shaniab999@gmail.com>
2026-05-08 18:09:38 +0200

    fix mistake and add new method for component social media and card hour header and footer

--- js/main.js
+++ js/main.js
@@ -36,8 +36,8 @@ function loadFooter(id) {
             footer.classList.add('footer--' + page);  // adapter,  le footer en fonction de la page
 
             loadNavBar('footer-navBar'); // Charger la Nav Bar dans le footer
-            loadSocialMedia('footer-socialMedia'); // Charger le bouton Facebook dans le footer
-            loadCardHour('footer-CardHour'); // Charger les horaires dans le footer
+            loadSocialMedia("footer-socialMedia", "footer"); // Charger le bouton Facebook dans le footer
+            loadCardHour("footer-cardHour", "footer"); // Charger les horaires dans le footer
             loadLogo('footer-logo', { // Charger le logo dans le footer
                 image: "../assets/logo.png"
             });
@@ -76,7 +76,7 @@ function loadNavBar(id) {
 }
 
 // LIGHT COLUMN
-//A mettre directement dans la nav bar et faire en sorte que la light column s'adapte a chauqe page (ex: light-column--home, light-column--contact, etc...)
+//A mettre directement dans la nav bar et faire en sorte que la light column s'adapte a chaque page (ex: light-column--home, light-column--contact, etc...)
 // a faire plus tard.
 function loadlightColumn(id) {
     fetch("../components/lightColumn.html")
@@ -144,26 +144,59 @@ document.querySelectorAll('.separate').forEach(el => {
 });
 
 // social media
-function loadSocialMedia(id) {
+function loadSocialMedia(id, variant, options) {
     fetch("../components/socialMedia.html")
         .then(res => res.text())
         .then(data => {
             document.getElementById(id).innerHTML = data;
+
+            //remplace variable du html
+            let html = data
+                .replace('{{facebook}}', options.facebook)
+                .replace('{{insta}}', options.insta)
+                .replace('{{tiktok}}', options.tiktok)
+
+            document.getElementById(id).innerHTML = html //inject composant
+
+            const container = document.getElementById(id); //récupère dans le document par l'id donc le container
+
+            const socialMedia = container.querySelector('social-media'); // récupère composant injecté
+
+            socialMedia.classList.add("social-media--" + variant); //ont ajoute au nom de class social-media un variant selon le context
+
         });
 }
+//Appels composants dans home
+loadSocialMedia("home-socialMedia", "home", {
+    facebook: "../assets/icon/facebook.png",
+    insta: "../assets/icon/instagram.png",
+    tiktok: "../assets/icon/tiktok.png"
+});
+
+//Appels  // // Footer
+loadSocialMedia("footer-socialMedia", "footer", {
+    facebook: "../assets/icon/facebook-white.png",
+    insta: "../assets/icon/instagram-white.png",
+    tiktok: "../assets/icon/tiktok-white.png"
+});
+
 
 
 // card hours
-function loadCardHour(id) {
+function loadCardHour(id, variant) {
     fetch("../components/cardHour.html")
         .then(res => res.text())
         .then(data => {
-            document.getElementById(id).innerHTML = data;
+            document.getElementById(id).innerHTML = data; // injecte
 
-            const cardHour = document.querySelector('.cardHour');
+            const container = document.getElementById(id); //récupère
+
+            const cardHour = container.querySelector('.card-hour'); //récupère
+
+            cardHour.classList.add("card-hour--" + variant);
         })
 }
-loadCardHour("cardHour");
+loadCardHour("contact-cardHour", "contact");
 
 
 
